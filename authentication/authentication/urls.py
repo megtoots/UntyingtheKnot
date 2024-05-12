6d67c2d5bcbc0002d859730bb9ed65033eb726f3
megtoots <tethraxrivera@gmail.com>
2024-05-13 01:46:08 +0800

    Updated with User Authentication and Login
    : + User User Authentication and Login
    : fixed redirecting links according to django

---
+++ authentication/authentication/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', user_views.register, name='register'),
+    path('login/', user_views.login_view, name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('main-page/', user_views.main_page, name='main_page'),
+    path('main-page/educational_materials.html', user_views.educational_materials, name='educational_materials'),
+    path('main-page/childhood.html', user_views.childhood, name='childhood'),
+    path('main-page/pastexp.html', user_views.past_exp, name='past_exp'),
+    path('main-page/currentc.html', user_views.currentc, name='currentc'),
+    path('main-page/healthandwell.html', user_views.currentc, name='healthandwell'),
+]
---
+++ authentication/users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created. You can log in now!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main_page')  # Redirect to main_page upon successful login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def main_page(request):
+    # Logic for rendering the main_page.html template
+    return render(request, 'users/main_page.html')
+def educational_materials(request):
+    return render(request, 'users/educational_materials.html')
+def past_exp(request):
+    return render(request, 'users/pastexp.html')
+def childhood(request):
+    return render(request, 'users/childhood.html')
+def currentc(request):
+    return render(request, 'users/currentc.html')
+def healthandwell(request):
+    return render(request, 'users/healthandwell.html')
